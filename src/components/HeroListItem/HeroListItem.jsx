c4300cd27779c930d12200395b1978b946465287
Lolita <lolita454462@gmail.com>
2025-06-25 15:51:01 +0200

    fix: images links

--- src/components/HeroListItem/HeroListItem.jsx
+++ src/components/HeroListItem/HeroListItem.jsx
@@ -3,7 +3,7 @@ import s from './HeroListItem.module.css';
 const HeroListItem = ({ img, img2x, title }) => {
     return (
         <a href="#">
-            <img className={s.img} src={`/images/${img}`} srcSet={`/public/images/${img2x}`} alt="Фото людини і кота" />
+            <img className={s.img} src={`/images/${img}`} srcSet={`/images/${img2x}`} alt="Фото людини і кота" />
             <div className={s.boxTitle}>
                 <h2 className={s.title}>{title}</h2>
             </div>
