eefafbedb020fc406aab45f68a71a8bbfa75fb78
Lolita <lolita454462@gmail.com>
2025-06-25 17:13:23 +0200

    add: aside section css

--- src/components/AsideList/AsideList.jsx
+++ src/components/AsideList/AsideList.jsx
@@ -1,9 +1,11 @@
 import AsideListItem from '../AsideListItem/AsideListItem';
 import { asideData } from '../../data/asideData';
 
+import s from './AsideList.module.css';
+
 const AsideList = () => {
     return (
-        <ul>
+        <ul className={s.list}>
             {asideData.map(item => (
                 <li key={item.id}>
                     <AsideListItem img={item.img} img2x={item.img2x} desc={item.description} title={item.title} />
--- src/components/AsideListItem/AsideListItem.jsx
+++ src/components/AsideListItem/AsideListItem.jsx
@@ -2,11 +2,12 @@ import s from './AsideListItem.module.css';
 
 const AsideListItem = ({ img, img2x, title, desc }) => {
     return (
-        <a href="#">
+        <a href="#" className={s.link}>
             <img className={s.img} src={`/images/${img}`} srcSet={`/images/${img2x}`} alt="Фото кота" />
-
-            <h2 className={s.title}>{title}</h2>
-            <p className={s.desc}>{desc}</p>
+            <div>
+                <h2 className={s.title}>{title}</h2>
+                <p className={s.desc}>{desc}</p>
+            </div>
         </a>
     );
 };
